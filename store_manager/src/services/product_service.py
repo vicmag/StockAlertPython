d2ec6ff4bfac61138938761efe97a1ba85d9a81e
Victor Marquez Garcia <vicmag@iMac.local>
2025-05-29 10:09:26 -0600

    Fase Verde

--- store_manager/src/services/product_service.py
+++ store_manager/src/services/product_service.py
@@ -3,6 +3,8 @@ from src.repositories.product_repository import ProductRepository
 class ProductService:
     def __init__(self, repository: ProductRepository):
         self.repository = repository
-        
+
     def increment_stock(self, product_name: str, amount: int) -> bool:
-        raise NotImplementedError("Not implemented yet")
+        product = self.repository.find_by_name(product_name)
+        product.stock += amount
+        return self.repository.save(product)
