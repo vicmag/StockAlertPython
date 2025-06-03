5912edd97f83433ed6450fb7148de8f68cc6e69a
vicmag <vmarquez@minsait.com>
2025-06-03 12:22:40 -0600

    Escenario 4. Fase Verde Completa

--- store_manager/src/services/product_service.py
+++ store_manager/src/services/product_service.py
@@ -5,5 +5,7 @@ class ProductService:
         self.repository = repository
 
     def increment_stock(self, name: str, increment: int) -> bool:
-        raise NotImplementedError("Fase Roja")
+        product = self.repository.find_by_name(name)
+        product.stock += increment
+        return self.repository.save(product)
         
