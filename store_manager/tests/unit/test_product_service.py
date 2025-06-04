4b8dee220470a75ec8db8a3e3626e84985020782
vicmag <vmarquez@minsait.com>
2025-06-04 12:36:01 -0600

    CA4. Nuevo ciclo RGR. Fase Roja

--- store_manager/tests/unit/test_product_service.py
+++ store_manager/tests/unit/test_product_service.py
@@ -44,5 +44,22 @@ class TestProductService:
         with pytest.raises(ValueError, match="Producto no encontrado"):
             service.increment_stock(nameProduct, increment)
 
+    def test_increment_stock_should_fail_if_increment_is_not_positivo(self, mocker):
+        # Arrange (configuración)
+        mock_repository = mocker.Mock()
+        nameProduct = "Camiseta"
+        initial_stock = 10
+        increment = -5
+        product = Product(name=nameProduct, stock=initial_stock)
+        
+        # Configuración del comportamiento del mock
+        mock_repository.find_by_name.return_value = product
+
+        service = ProductService(mock_repository)
+
+        # Act & Assert (ejecucción y validación)
+        with pytest.raises(ValueError, match="El incremento no debe ser negativo"):
+            service.increment_stock(nameProduct, increment)
+
         
 
